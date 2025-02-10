da0cf46dbc8ead604ac8519409bff70b7ad3443f
kristimengri <kristimengri@gmail.com>
2025-02-10 20:14:08 +0100

    fix

--- src/main/kotlin/com/car/CarRenting/CarRentingApplication.kt
+++ src/main/kotlin/com/car/CarRenting/CarRentingApplication.kt
@@ -1,10 +1,12 @@
 package com.car.CarRenting
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
+
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
@@ -14,6 +16,7 @@ class CarRentingApplication
 
 fun main(args: Array<String>) {
     runApplication<CarRentingApplication>(*args)
+
 }
 
 
